6e12ce239f59039c024bdf92b21e0afc3655c3f0
PabloC2A <pabloandrescriolloalvarez@gmail.com>
2025-05-27 09:14:23 -0500

    Consulta 2

---
+++ consulta02.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import sessionmaker
+from clases import Entrega, Tarea, Curso, Departamento, engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Obtener todos los departamentos que tengan entregas con nota <= 0.3.
+# Por cada objeto Departamento:
+# - Nombre del departamento
+# - Número de cursos que tiene ese departamento
+# distinct() = sirve para eliminar duplicados si hay múltiples entregas
+
+departamentos_bajas_calificaciones = session.query(Departamento) \
+    .join(Departamento.cursos) \
+    .join(Curso.tareas) \
+    .join(Tarea.entregas) \
+    .filter(Entrega.calificacion <= 0.3) \
+    .distinct() \
+    .all()
+
+# Imprimir los resultados obteneidos
+for departamento in departamentos_bajas_calificaciones:
+    print(f"Departamento: {departamento.nombre}, Cursos: {len(departamento.cursos)}")
